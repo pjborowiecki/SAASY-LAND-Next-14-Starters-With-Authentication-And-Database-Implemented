65fe40f0877a0b402916ee5036f24014808e5eeb
P J Borowiecki <piotr.borowiecki@yahoo.com>
2024-02-09 10:31:42 +0100

    Fix the scroll on mobile devices; fix user avatar sizing error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
     <html lang="en">
       <body
         className={cn(
-          "w-full overflow-hidden bg-background bg-gradient-to-r from-background to-pink-400/10 font-sans antialiased",
+          "w-full !overflow-scroll overflow-x-hidden bg-background bg-gradient-to-r from-background to-pink-400/10 font-sans antialiased",
           fontInter.variable,
           fontUrbanist.variable,
           fontHeading.variable
